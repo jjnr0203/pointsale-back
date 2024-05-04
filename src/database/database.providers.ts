747322b8bc5ab0784485be6eca2c64f266ab79d9
migue1angel <macf06200@gmail.com>
2024-05-04 10:27:08 -0500

    app module correccion

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -14,6 +14,7 @@ export const databaseProviders = [
         entities: [
             __dirname + '/../**/*.entity{.ts,.js}',
         ],
+        //dropSchema:true,
         synchronize: true,
       });
 
