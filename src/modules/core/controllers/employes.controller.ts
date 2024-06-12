f52363b13e029ff04791d1c6de16244d0ec7c965
DILANIUP <dilanurquizo29112004@gmail.com>
2024-06-11 22:20:02 -0500

    employee controlador

---
+++ src/modules/core/controllers/employes.controller.ts
@@ -0,0 +1,43 @@
+import { Body, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post } from "@nestjs/common";
+import { EmployesService } from "../services/employes.service";
+import { EmployeeEntity } from "../entities/employee.entity";
+import { EmployeDto } from "../dto/employee.dto";
+
+
+export class EmployeController {
+    constructor(private employesService: EmployesService) { }
+
+    @Get()
+    async findAll() {
+        return this.employesService.findAll();
+    }
+
+    @Get(':id')
+    async findOne(@Param('id') id: string) {
+        const data = this.employesService.findOne(id);
+        return {
+            date: Date,
+            message: 'Empleados encontrados'
+        }
+    }
+
+    @Post()
+    async create(@Body() payload: EmployeDto) {
+        const serviceResponse = await this.employesService.create(payload);
+        return {
+            data: serviceResponse,
+            message: 'Empleado creado'
+        }
+    }
+
+    
+
+    @Delete(':id')
+    async delete(@Param('id') id: string) {
+        const serviceResponse = await this.employesService.remove(id)
+        return {
+            data: serviceResponse,
+            message: 'Empleado eliminado'
+        }
+    }
+}
