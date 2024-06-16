5a6178b3c687ffb553d216d00eb65a017d71b631
j0n4t8an <morajonathanjm24@gmail.com>
2024-06-16 00:10:36 -0500

    adaptacion token

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -1,13 +1,21 @@
-import {Body, Controller, Post} from '@nestjs/common';
-import {AuthService} from '../services/auth.service';
-import {LoginDto} from "../dto/login.dto";
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AuthService } from '../services/auth.service';
+import { LoginDto } from '../dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-    @Post('login')
-    async login(@Body() payload: LoginDto) {
-        return await this.authService.validateUser(payload)
-    }
+  @Post('login')
+  async login(@Body() payload: LoginDto) {
+    const serviceResponse = await this.authService.validateUser(payload);
+    return {
+      data: serviceResponse,
+    };
+  }
+
+  @Get('profile')
+  getProfile() {
+    return 'Tiene acceso a una ruta restringida';
+  }
 }
